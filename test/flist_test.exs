f058967f86aebb7136dcccbf19b098ece3fc2e31
SchrodingerZhu <admin@myforesight.xyz>
2017-07-01 21:20:52 +0800

    Format with emacs

--- test/flist_test.exs
+++ test/flist_test.exs
@@ -13,12 +13,12 @@ defmodule FListTest do
     data = initData()
     assert data == (data |> FList.fromList() |> FList.toList())
   end
-
+  
   test "Test snoc" do
     data = initData()
     assert data |> List.foldl(FList.new(), fn (x, acc) -> FList.snoc(acc, x) end) |> FList.toList()
   end
-
+  
   test "Test concat" do
     x = initData()
     y = initData()
@@ -28,33 +28,32 @@ defmodule FListTest do
 	  data = initData()
 	  l = FList.fromList(data)
     result= 
-       Stream.repeatedly(fn -> Enum.random(0..99999) end)
-	     |> Enum.take(100)
-	     |> Enum.map(fn a -> Enum.fetch!(data, a) == FList.getAt(l, a) end)
-	     |> List.foldl(true, fn (x, acc) -> x && acc end)
+      Stream.repeatedly(fn -> Enum.random(0..99999) end)
+	  |> Enum.take(100)
+	  |> Enum.map(fn a -> Enum.fetch!(data, a) == FList.getAt(l, a) end)
+	  |> List.foldl(true, fn (x, acc) -> x && acc end)
     assert result 
   end
-
+  
   test "Test update" do
   	data = initData()
     l = FList.fromList(data)
 	  {nl, fl} = 
-        Stream.repeatedly(fn -> {Enum.random(0..99999), Enum.random(1..100000)} end)
-	      |> Enum.take(100)
-        |> List.foldl({data, l}, fn ({i, x}, {a, b}) -> {List.update_at(a, i, fn _ -> x end), FList.setAt(b, i, x)} end)
+      Stream.repeatedly(fn -> {Enum.random(0..99999), Enum.random(1..100000)} end)
+	  |> Enum.take(100)
+    |> List.foldl({data, l}, fn ({i, x}, {a, b}) -> {List.update_at(a, i, fn _ -> x end), FList.setAt(b, i, x)} end)
 	  assert nl == FList.toList(fl)
   end
-
+  
   
   test "Test moveToFront" do
-	data = initData(1000)
-       l = FList.fromList(data)
-	{nl, fl} =
-	Stream.repeatedly(fn -> Enum.random(0..999) end)
-	|> Enum.take(100)
-	|> List.foldl({data, l}, fn (i, {a, b}) -> {mtf(a, i), FList.moveToFront(b, i)} end)
-	assert nl == FList.toList(fl)
+	  data = initData(1000)
+    l = FList.fromList(data)
+	  {nl, fl} =
+	    Stream.repeatedly(fn -> Enum.random(0..999) end)
+	  |> Enum.take(100)
+	  |> List.foldl({data, l}, fn (i, {a, b}) -> {mtf(a, i), FList.moveToFront(b, i)} end)
+	  assert nl == FList.toList(fl)
   end
   
 end
-  
